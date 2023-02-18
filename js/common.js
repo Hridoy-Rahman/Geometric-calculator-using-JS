2d2bc36dfa14c12d3fc598e591b7559009e6497f
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-02-18 18:07:06 +0600

    Mouse Hover color change done

--- js/common.js
+++ js/common.js
@@ -1,9 +1,43 @@
 // Getting Input Element Function
-function getInputElementById(elementId){
+function getInputValueById(elementId){
     const inputField=document.getElementById(elementId);
     const inputFieldString=inputField.value;
     const inputValue=parseFloat(inputFieldString);
+    inputField.value='';
+    if(isNaN(inputValue) || inputValue==''){
+        alert('Please Enter Number.');
+    }
+    
     return inputValue;
 }
 
-// getting Text Element Function
+
+// Showing The Data
+function  displayData(name,area){
+
+    const tr=document.createElement('tr');
+    const container=document.getElementById('table-body');
+
+    tr.innerHTML=`
+    <td><ol><li>$. ${name}</li></ol></td>
+    <td>${area}cm<sup>2</sup></td>
+    <td><button id="btn-convert"
+    class="w-10/12 bg-sky-400 text-white text-xl rounded">m<sup>2</sup></button></td></>
+    `;
+    tr.style.marginBottom="100px";
+   
+    container.appendChild(tr);
+
+}
+
+
+// Color Change Function
+function colorChange(elementId){
+    const hover=document.getElementById(elementId);
+    hover.style.backgroundColor='rgb('+Math.round(Math.random() *255) +
+    ','+Math.round(Math.random() *255) +
+    ',' +Math.round(Math.random() *255) + ')';
+}
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,49 +1,85 @@
 
 // Triangle Area
 document.getElementById('triangle-calculate').addEventListener('click',function(){
-    const triangleBredth=getInputElementById('triangle-bredth');
-    const triangleHeight=getInputElementById('triangle-height');
-    const triangleArea = 0.5 * triangleBredth * triangleHeight;
-    console.log(triangleArea);
+    const triangleBredth=getInputValueById('triangle-bredth');
+    const triangleHeight=getInputValueById('triangle-height');
+    const triangleArea = (0.5 * triangleBredth * triangleHeight).toFixed(2);
+    
+    const name=document.getElementById('triangle').innerText;
+    const area=triangleArea;
+   
+    displayData(name,area,)
+
     
 })
 
+
 // Rectangle Area
 document.getElementById('rectangle-calculate').addEventListener('click',function(){
-    const rectangleWidth=getInputElementById('rectangle-width');
-    const rectangleHeight=getInputElementById('rectangle-height');
-    const rectangleArea = rectangleHeight * rectangleWidth;
-    console.log(rectangleArea);
+    const rectangleWidth=getInputValueById('rectangle-width');
+    const rectangleHeight=getInputValueById('rectangle-height');
+    const rectangleArea = (rectangleHeight * rectangleWidth).toFixed(2);
+    
+    const name=document.getElementById('rectangle').innerText;
+    const area=rectangleArea;
+   
+
+    displayData(name,area,)
+
 })
 // parallelogram Area
 document.getElementById('parallelogram-calculate').addEventListener('click',function(){
-    const parallelogramBredth=getInputElementById('parallelogram-width');
-    const parallelogramHeight=getInputElementById('parallelogram-height');
-    const parallelogramArea = parallelogramBredth * parallelogramHeight;
-    console.log(parallelogramArea);
+    const parallelogramBredth=getInputValueById('parallelogram-width');
+    const parallelogramHeight=getInputValueById('parallelogram-height');
+    const parallelogramArea = (parallelogramBredth * parallelogramHeight).toFixed(2);
+    
+    const name=document.getElementById('parallelogram').innerText;
+    const area=parallelogramArea;
+   
+
+    displayData(name,area,)
+
 })
 // Rhombus Area
 
 document.getElementById('rhombus-calculate').addEventListener('click',function(){
-    const rhombusCorner1=getInputElementById('rhombus-d1');
-    const rhombusCorner2=getInputElementById('rhombus-d2');
-    const rhombusArea = 0.5 * rhombusCorner1 * rhombusCorner2;
-    console.log(rhombusArea);
+    const rhombusCorner1=getInputValueById('rhombus-d1');
+    const rhombusCorner2=getInputValueById('rhombus-d2');
+    const rhombusArea = (0.5 * rhombusCorner1 * rhombusCorner2).toFixed(2);
+   
+    const name=document.getElementById('rhombus').innerText;
+    const area=rhombusArea;
+   
+
+    displayData(name,area,)
+
 })
 
 // pentagon
 document.getElementById('pentagon-calculate').addEventListener('click',function(){
-    const pentagonP=getInputElementById('pentagon-p');
-    const pentagonB=getInputElementById('pentagon-b');
-    const pentagonArea = 0.5 * pentagonP * pentagonB;
-    console.log(pentagonArea);
+    const pentagonP=getInputValueById('pentagon-p');
+    const pentagonB=getInputValueById('pentagon-b');
+    const pentagonArea =( 0.5 * pentagonP * pentagonB).toFixed(2);
+   
+    const name=document.getElementById('pentagon').innerText;
+    const area=pentagonArea;
+   
+
+    displayData(name,area,)
+
 })
 // Ellipse
 document.getElementById('ellipse-calculate').addEventListener('click',function(){
     const pi=3.14;
-    const ellipseA=getInputElementById('ellipse-a');
-    const ellipseB=getInputElementById('ellipse-b');
-    const ellipseArea = pi * ellipseA * ellipseB;
-    console.log(ellipseArea);
+    const ellipseA=getInputValueById('ellipse-a');
+    const ellipseB=getInputValueById('ellipse-b');
+    const ellipseArea = (pi * ellipseA * ellipseB).toFixed(2);
+    
+    const name=document.getElementById('ellipse').innerText;
+    const area=ellipseArea;
+   
+
+    displayData(name,area,)
+
 })
 
